5b96d80a130d6780b8f02b5c85507446bc8cda99
“a2218812990 <>
2020-03-09 20:34:04 +0800

    主页轮播图

--- src/pages/Home/Default/index.js
+++ src/pages/Home/Default/index.js
@@ -1,11 +1,68 @@
 import React, { Component } from 'react'
+import { Carousel} from 'antd-mobile'; 
 
+import {BASE_URL} from '../../../utils/request'
+import {getSwiper} from '../../../utils/API/home'
  class Default extends Component {
+    state = {
+        data: [],
+        imgHeight: 212,
+        autoplay:false
+      }
+
+      componentDidMount() {
+        this.getSwiper()
+      }
+    //   轮播图组件
+    Swipers(){
+        return(
+            this.state.data.map(val => (
+                <a
+                  key={val.id}
+                  href="http://www.ithema.com"
+                  style={{ display: 'inline-block', width: '100%', height: this.state.imgHeight }}
+                >
+                  <img
+                    src={`${BASE_URL}${val.imgSrc}`}
+                    alt=""
+                    style={{ width: '100%', verticalAlign: 'top' }}
+                    onLoad={() => {
+                      // fire window resize event to change height
+                      window.dispatchEvent(new Event('resize'));
+                      this.setState({ imgHeight: 'auto' });
+                    }}
+                  />
+                </a>
+              ))
+        )
+    }
+    //  获取轮播图
+    getSwiper=async ()=>{
+        let res= await getSwiper()
+        console.log(res);
+        if(res.status===200){
+             this.setState({
+                data:res.data
+             },()=>{
+                this.setState({
+                    autoplay:true
+                 })
+             })
+        }
+    }
+
     render() {
         return (
-            <div>
-                默认1111111s
-            </div>
+         
+            <Carousel
+              autoplay={this.state.autoplay}
+              infinite
+              beforeChange={(from, to) => console.log(`slide from ${from} to ${to}`)}
+              afterChange={index => console.log('slide to', index)}
+            >
+              {this.Swipers()}
+            </Carousel>
+          
         )
     }
 }
---
+++ src/utils/API/home.js
@@ -0,0 +1,8 @@
+// 主页模块封装请求方法
+import request from '../request'
+// 获取轮播图
+export  const getSwiper= ()=> request.get('/home/swiper')
+    
+
+
+
